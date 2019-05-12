5f563bfc60b6486dd56cbd77b818241d29cfb514
Guillaume Piolat <contact@auburnsounds.com>
2019-05-12 15:21:58 +0200

    parses and output comments ability to list only signature ability to select intrinsics by name or instruction sets ("ALL" supported) display count of selected intrinsics, useful to know if some have been added added a command-line and usage information build on 32-bit

--- source/output.d
+++ source/output.d
@@ -1,6 +1,7 @@
 import std.algorithm : map;
 import std.format : format, formattedWrite;
-
+import std.string;
+import std.stdio;
 import lexer;
 import parser;
 import dwriter;
@@ -15,7 +16,8 @@ struct Param {
 	}
 }
 
-struct Func {
+struct Func 
+{
 	string name;
 	string tech;
 	Param ret;
@@ -28,27 +30,52 @@ struct Func {
 	}
 }
 
-string toD(Func f) {
+string printIntrinsicAsDBody(Func f) {
 	import std.array : appender;
 	auto app = appender!string();
-	toD(app, f);
+	printIntrinsicAsDBody(app, f);
 	return app.data;
 }
 
-void toD(Out)(ref Out o, Func f) {
-	formattedWrite!"/*%s*/\n"(o, f.operation);
-	formattedWrite!"%s %s(%-(%s, %)) {\n"(o, f.ret.type, f.name,
+string indent(string s)
+{
+    return s.replace("\n", "\n    ");
+}
+
+void printIntrinsicAsDSignature(Out)(ref Out o, Func f) 
+{ 
+    formattedWrite!"%s %s(%-(%s, %)) pure nothrow @nogc @safe;\n"(o, f.ret.type, f.name,
+                                                                f.params.map!(a => format("%s %s", a.type, a.name)));
+}
+
+void printIntrinsicAsDBody(Out)(ref Out o, Func f) 
+{
+    if (f.desc)
+        formattedWrite!"/// %s\n"(o, f.desc);
+	formattedWrite!"%s %s(%-(%s, %)) pure nothrow @nogc @safe\n{\n"(o, f.ret.type, f.name,
 			f.params.map!(a => format("%s %s", a.type, a.name)));
-	auto l = Lexer(f.operation);
-	auto parser = Parser(l);
-	try {
-		auto d = parser.parseStart();
-		DWriter!Out vis = new DWriter!Out(o);
-		vis.accept(cast(const(Start))d);
-	} catch(Throwable e) {
-		formattedWrite!"/+\n%s+/\n"(o, e.msg);
-		formattedWrite!"\tassert(false, \"Failed to parse\");\n"(o);
-	} finally {
-		formattedWrite!"}\n\n"(o);
-	}
+    if (f.operation)
+    {
+	    auto l = Lexer(f.operation);
+	    auto parser = Parser(l);
+	    try {
+		    auto d = parser.parseStart();
+		    DWriter!Out vis = new DWriter!Out(o);
+		    vis.accept(cast(const(Start))d);
+	    } 
+        catch(Throwable e) 
+        {
+            // didn't parse, put the operation body
+            string operationContent = format("    /*%s*/\n", f.operation);
+            formattedWrite!"%s"(o, operationContent.indent);
+
+            string parseErrorMessage = format("//%s\n", e.msg);
+            formattedWrite!"%s"(o, parseErrorMessage.indent);
+
+            formattedWrite!"assert(false, \"Failed to parse\");\n"(o);
+	    } 
+        formattedWrite!"}\n\n"(o);
+    }
+    else
+        throw new Exception("Intrinsic has no pseudo-code");
 }
